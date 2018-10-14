0e7b92ed0b78e190d3ee76ea48e45a299880449c
Rahul <snakepowerpoint@gmail.com>
2018-10-14 23:50:18 +0800

    compute Taylor's exponent

--- script/taylor_exponent.r
+++ script/taylor_exponent.r
@@ -1,21 +1,25 @@
-load(file="C:\\Users\\b9930\\Google ¶³ºÝµwºÐ\\publication\\SpatialVariability\\data\\cpue.RData")
+wd = "C:\\Users\\b9930\\Google ¶³ºÝµwºÐ\\publication\\SpatialVariability\\"
+#load(file=paste0(wd, "data\\cpue.RData")
+setwd(paste0(wd, "data\\"))
+
+cpue = read.csv("compiled_cpue_data.csv", header = TRUE)
+cpue = split(cpue, f = cpue$Species)
 
 ### Taylor's power law
 # compute total CPUE and mean and variance of CPUE
-taylor <- lapply(cpue.species.drop0, FUN=function(y){
+taylor <- lapply(cpue, FUN=function(y){
     data = data.frame(y)
-    result = with(data, aggregate(x, by=list(Year=Year, Quarter=Quarter), FUN=function(z){var(z)}))
-    result1 = with(data, aggregate(x, by=list(Year=Year, Quarter=Quarter), FUN=function(z){mean(z)}))
-    result2 = with(data, aggregate(x, by=list(Year=Year, Quarter=Quarter), FUN=sum))
-    cbind(result, result1$x, result2$x)
+    cpue.var = with(data, aggregate(CPUE, by=list(Year=Year, Quarter=Quarter), FUN=function(z){var(z)}))
+    cpue.mean = with(data, aggregate(CPUE, by=list(Year=Year, Quarter=Quarter), FUN=function(z){mean(z)}))
+    cpue.sum = with(data, aggregate(CPUE, by=list(Year=Year, Quarter=Quarter), FUN=sum))
+    
+    out = cbind(cpue.var, cpue.mean$x, cpue.sum$x)
+    
+    # change the column names and sort the data
+    colnames(out) = c("Year", "Quarter", "Var.CPUE", "Mean.CPUE", "Total.CPUE")
+    out[with(out, order(Year, Quarter)), ]
 })
 
-# change the column names and sort the data
-taylor <- lapply(taylor, FUN=function(y){
-    data = data.frame(y)
-    colnames(y) = c("Year", "Quarter", "Var.CPUE", "Mean.CPUE", "Total.CPUE")
-    y[with(y, order(Year, Quarter)), ]
-})
 
 # now we have variance and mean of CPUE, and total CPUE for each cruise for each species
 # remove years with 0 mean or variance because log(0) will cause error
@@ -43,4 +47,13 @@ taylor.coeff = lapply(taylor, FUN=function(y){
     return(output)
 })
 
-save.image(file="C:\\Users\\b9930\\Google ¶³ºÝµwºÐ\\publication\\SpatialVariability\\data\\taylor.RData")
+taylor.dataframe = do.call(rbind, taylor.coeff)
+taylor.dataframe$Species = rownames(taylor.dataframe)
+rownames(taylor.dataframe) = NULL
+taylor.dataframe = cbind(Species = taylor.dataframe$Species, 
+                         taylor.dataframe[, -dim(taylor.dataframe)[2]])
+
+write.csv(taylor.dataframe, file=paste0(wd, "output\\taylor.csv"), row.names=FALSE)
+
+
+#save.image(file="C:\\Users\\b9930\\Google ¶³ºÝµwºÐ\\publication\\SpatialVariability\\data\\taylor.RData")
