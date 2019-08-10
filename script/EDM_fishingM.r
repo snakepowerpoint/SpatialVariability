5fffc2bafc4f5ed12b9c21de3257626665f0b0a5
Rahul <snakepowerpoint@gmail.com>
2019-08-10 15:10:46 +0800

    complete CCM and S-map analysis

--- script/EDM_fishingM.r
+++ script/EDM_fishingM.r
@@ -1,115 +1,8 @@
-library(Rcpp)
-library(devtools)
-library(rEDM)
-
-
-
 ### Data preparation
 wd = "C:\\Users\\b9930\\Google ¶³ºÝµwºÐ\\publication\\SpatialVariability\\"
-setwd(paste0(wd, "output"))
+setwd(paste0(wd, "script"))
+source("EDM.r")
 
-species = c("Clupea harengus", "Gadus morhua", "Melanogrammus aeglefinus",
-            "Merlangius merlangus", "Pleuronectes platessa", "Pollachius virens",
-            "Scomber scombrus", "Sprattus sprattus", "Trisopterus esmarkii")
-
-# load data
-EDM_list = list()
-for (i in 1:length(species)){
-    EDM_list[[i]] = list()
-    EDM_list[[i]]$species = species[i]
-    EDM_list[[i]]$data = read.csv(paste0(species[i], ".csv"), header = TRUE)
-}
-names(EDM_list) = species
-
-# standarization
-EDM_list = lapply(EDM_list, FUN = function(item){
-    item$data_std = cbind(item$data[, c(1,2)], scale(item$data[, -c(1,2)]))
-    
-    return(item)
-})
-
-# change column name
-EDM_list = lapply(EDM_list, function(item){
-    var.name = c("Year", "Quarter", "CV.CPUE", "AgeDiversity", "Abundance", 
-                 "AMO", "SBT", "CVofSBT", "SST", "CVofSST")
-    names(item$data) = var.name
-    names(item$data_std) = var.name
-    
-    return(item)
-})
-
-# drop discontinuous data
-EDM_list$`Pleuronectes platessa`$data_std = EDM_list$`Pleuronectes platessa`$data_std[19:48, ]
-
-# a function to compute mean value for each year
-compute_mean_per_year = function(data){
-    data_wo_year = subset(data, select=-c(Year, Quarter))
-    mean_per_year = aggregate(data_wo_year, by=list(Year=data$Year), FUN=mean)
-    return(mean_per_year)
-}
-
-# apply above function to each item (species) in list
-EDM_list = lapply(EDM_list, FUN=function(item){
-    data_std = item$data_std
-    data_per_year = compute_mean_per_year(data_std)
-    item$data_per_year = data_per_year
-    return(item)
-})
-
-# load fishing mortality data
-setwd(paste0(wd, "output"))
-fishingM = read.csv("fishingM.csv", header=TRUE)
-fishingM_wo_species = subset(fishingM, select=-c(Species))
-fishingM_wo_species = split(fishingM_wo_species, f=fishingM$Species)
-
-# standardization
-fishingM_wo_species = lapply(fishingM_wo_species, FUN=function(item){
-    item$F = as.vector(scale(item$F))
-    return(item)
-})
-
-# combined data with fishing mortality data
-for (i in 1:length(EDM_list)){
-    result = merge(EDM_list[[i]]$data_per_year, fishingM_wo_species[[i]], by=c("Year"))
-    EDM_list[[i]]$data_per_year = result
-}
-
-
-
-### a function returning all local peaks
-# (will be applied to get all local optimal embedding dimensions)
-detectPeak = function(data){
-    n = length(data)
-    peaks = c()
-    rho = c()
-    for (idx in 2:(n-1)){
-        if ((data[idx-1] < data[idx]) & (data[idx+1] < data[idx])){
-            peaks = c(peaks, idx)
-            rho = c(rho, data[idx])}
-    }
-    output = data.frame(peaks, rho)
-    
-    return(output[order(output$rho, decreasing = TRUE), ])
-}
-
-### a function using simplex projection to determine embedding dimension for each variable
-computeEmbedding = function(data, show_result=TRUE){
-    # create a list to record embedding dimensions
-    dim.list = list()
-    
-    # determine embedding dimension of each variables
-    for (column in colnames(data)){
-        output <- simplex(data[[column]], E = 1:10, tau = 1, tp = 1) # embedding dimension 
-        if (show_result){
-            plot(output$E, output$rho, type = "l", main = column, lwd = 2,
-                 xlab = "Embedding Dimension (E)", ylab = "Forecast Skill (rho)")
-        }
-        dim.list[[column]] = detectPeak(output$rho)
-        #dim.list[[column]] = output[, c("E", "rho")][order(output$rho, decreasing = TRUE), ]
-    }
-    
-    return(dim.list)
-}
 
 EDM_list = lapply(EDM_list, FUN = function(item){
     data = subset(item$data_per_year, select=-c(Year))
@@ -131,8 +24,7 @@ determineCausality = function(
     dim.list, 
     species,
     lags = 3, 
-    num_samples = 100, 
-    show_result = FALSE
+    num_samples = 100
 ){
     cols = c('length', 'lib.size', 'E', 'library', 'target', 'tar.lag', 
              'rho', 'sd.rho', 'kendall.tau', 'significance')
@@ -177,8 +69,7 @@ EDM_list = lapply(EDM_list, function(item){
     
     ccm_result = determineCausality(data = data, 
                                     dim.list = item$E, 
-                                    species = item$species,
-                                    show_result = FALSE)
+                                    species = item$species)
     item$ccm = ccm_result
     
     return(item)
@@ -186,7 +77,6 @@ EDM_list = lapply(EDM_list, function(item){
 
 
 # ggplot function to plot CCM results
-library(ggplot2)
 setwd(paste0(wd, 'script'))
 source("utils/plot.r")
 
@@ -204,6 +94,14 @@ for (item in EDM_list){
     plot_ccm_result(item$ccm, 'Abundance', item$species)
 }
 
+
+
+### CCM analysis to find causal variables of age diversity and abundance
+# sort data frame: first are affected var
+
+
+
+### Apply S-map on age diversity and abundance
 
 
 ### Appendix
