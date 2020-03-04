0fd52f29f86d8985356334eee110892acf807687
Rahul <snakepowerpoint@gmail.com>
2020-03-04 11:26:16 +0800

    correct hyper-parameters

--- script/config.r
+++ script/config.r
@@ -1,9 +1,9 @@
 ## config 
 dataset = "data_std"  # change to "data_per_year" if fishing mortality is included
 detrend_fun = detrend_sig
-lags = 4  # change to "4" if fishing mortality is included
+lags = 8  # change to "4" if fishing mortality is included
 time_ccm = 200
-is_full = FALSE  # change to "TRUE" if a full time series is used in S-map analysis
+is_full = FALSE 
 
 library_var = "CV.CPUE"
 #library_var = "AgeDiversity"
