126f90cd9ee6e7ee8d4ec30110e9187a40d006ad
Rahul <snakepowerpoint@gmail.com>
2020-04-12 00:05:27 +0800

    move plotting parameters from config file to EDM utility file

--- script/config_cv.r
+++ script/config_cv.r
@@ -23,15 +23,3 @@ dir.create(file.path(wd, smap_path), showWarnings = FALSE)
 ## path for the results of robustness tes
 robust_path = paste0("output\\robustness_test\\", library_var, "_sig_detrend\\")
 dir.create(file.path(wd, robust_path), showWarnings = FALSE)
-
-
-## plot S-map coefficients
-# give each variable a unique color and shape
-# variable: c('F', 'spatial CV', 'age diversity','abundance',
-#             'AMO', 'SBT'(SST), 'CV of BT'(CV of SST))
-# color: black, yellow, red, green, blue, light blue, purple : c(1,7,2,3,4,5,6)
-# shape: crossbox, cross, circle(s), circle, diamond(s), triangle(s), triangle: c(7,4,21,1,23,24,2)
-variables = c("F", "CV.CPUE", "AgeDiversity", "Abundance", "AMO", 
-              "SBT", "CVofSBT", "SST", "CVofSST")
-cl = c(1,7,2,3,4,5,6,5,6)
-sh = c(7,4,21,1,23,24,2,24,2)
--- script/config_cv_F.r
+++ script/config_cv_F.r
@@ -24,14 +24,3 @@ dir.create(file.path(wd, smap_path), showWarnings = FALSE)
 robust_path = paste0("output\\robustness_test\\", library_var, "_sig_detrend_F\\")
 dir.create(file.path(wd, robust_path), showWarnings = FALSE)
 
-
-## plot S-map coefficients
-# give each variable a unique color and shape
-# variable: c('F', 'spatial CV', 'age diversity','abundance',
-#             'AMO', 'SBT'(SST), 'CV of BT'(CV of SST))
-# color: black, yellow, red, green, blue, light blue, purple : c(1,7,2,3,4,5,6)
-# shape: crossbox, cross, circle(s), circle, diamond(s), triangle(s), triangle: c(7,4,21,1,23,24,2)
-variables = c("F", "CV.CPUE", "AgeDiversity", "Abundance", "AMO", 
-              "SBT", "CVofSBT", "SST", "CVofSST")
-cl = c(1,7,2,3,4,5,6,5,6)
-sh = c(7,4,21,1,23,24,2,24,2)
